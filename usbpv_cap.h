71f891811ad01d10382e712e6e4dbd8c47392c86
xtoolbox <admin@xtoolbox.org>
2020-02-09 17:20:51 +0800

    add function comment

--- usbpv_cap.h
+++ usbpv_cap.h
@@ -13,13 +13,37 @@
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
+
+/** Wait or get a pipe transfer state
+ *
+ *  \ingroup Group_Host
+ *
+ *  \param[in] pipe           Pipe pointer, initial by \ref tusb_pipe_open
+ *  \param[in] timeout        0 - don't wait, just get the current pipe state
+ *                            0xffffffff - wait forever
+ *  \return    Channel state, type is \ref channel_state_t
+ */
+
+/** callback when got packet
+ * 
+ * \param[in] context      passed from usbpv_open
+ * \param[in] ts           timestamp in second
+ * \param[in] nano         timestamp in nano second
+ * \param[in] data         packet data
+ * \param[in] len          packet length
+ * \param[in] status       status < 0, means error, data parameter holds error description, the capture thread will stop on error
+ *                         status >= 0 means success
+ * 
+ * \return not used
+ */
 typedef long  (__cdecl* pfn_packet_handler)(void* context, unsigned long ts, unsigned long nano, const void* data, unsigned long len, long status);
 
-
 // option format
+// "<TYPE,NAME[,PARAM[,PARAM[,PARAM]]]><TYPE,NAME[,PARAM[,PARAM[,PARAM]]]>..."
+// valid TYPE:   select/text/int/file
+// option result format
+// <NAME,DATA><NAME,DATA>...
 
-// "<select,name1,option1[,option2[,option3]]><text,name2[,defaultValue]><int,name3[,defaultValue[,1[,100]]]><file,name4,defaultName[,demo file (*.demo)]>"
-// data in [] means optional
 // example:
 // "<select,name1,option1,option2,option3><text,name2,hello><int,name3,23,1,100><file,name4,123.test,Test file(*.test)>"
 // there are 4 option
@@ -31,11 +55,31 @@ typedef long  (__cdecl* pfn_packet_handler)(void* context, unsigned long ts, uns
 //  The result option pass to usbpv_open may
 //  "<name1,option2><name2,world><name3,14><name4,xxx.test>"
 
-
+/** get capture option
+ * 
+ * \param[out] option    capture option
+ * \param[in]  length    option buffer length
+ * 
+ * \return actual  option content length
+ */
 USBPV_API long  __cdecl usbpv_get_option(char* option, long length);
 
+/** open capture
+ * 
+ * \param[in]  option
+ * \param[in]  context used for callback
+ * \param[in]  callback function when packet received
+ * 
+ * \return capture handler, null for fail, more error info can be set by callback
+ */
 USBPV_API void* __cdecl usbpv_open(const char* option, void* context, pfn_packet_handler callback);
 
+/** close capture
+ * 
+ * \param[in]  handle   return bu usbpv_open
+ * 
+ * \return not used
+ */
 USBPV_API long  __cdecl usbpv_close(void* handle);
 
 
